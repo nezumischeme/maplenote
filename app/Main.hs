688c49c827aecf9da2fda998c4a5785c9c534840
bee <batquartet@gmail.com>
2025-08-15 14:09:25 -0400

    Make it compile

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified Maple as M
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = M.run
--- src/Maple.hs
+++ src/Maple.hs
@@ -1,13 +1,14 @@
-module Maple (app) where
+module Maple where
 
 import Text.Blaze.Html
 import Servant.API
 import Servant.HTML.Blaze
 import Servant.Server
+import Servant
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import Crypto.BCrypt
-
+import Network.Wai.Handler.Warp
+import System.IO
 type Index = "index"
   :> Get '[HTML] Html
 
@@ -18,5 +19,17 @@ index = pure . H.docTypeHtml $ do
   H.title "Login"
   H.p (H.a H.! A.href "/login" $ "Login")
 
-app :: IO ()
-app = putStrLn "Hello world!"
+run :: IO ()
+run = do
+  let port = 3000
+      settings =
+        setPort port $
+        setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
+        defaultSettings
+  runSettings settings =<< mkApp
+
+appApi :: Proxy Index
+appApi = Proxy
+
+mkApp :: IO Application
+mkApp = return $ serve appApi index 
